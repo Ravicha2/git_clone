f9d1a34750327a422c9fa1b32015b3d945e93741
Ravicha <palm.ravicha@outlook.com>
2025-07-29 21:12:28 +1000

    add removed file to remove from index

--- mygit-commit.py
+++ mygit-commit.py
@@ -44,16 +44,19 @@ def commit_log():
         for file in files:
             snapshot.writelines(file+"\n")
 
+
 def add_to_HEAD(filename, hash):
     head_path = ".mygit/HEAD"
     new_version = True
+    
 
     if os.path.exists(head_path):
-        with open(head_path, 'r') as head:
+        with open(".mygit/HEAD", 'r') as head:
             cached = head.readlines()
-            for line in cached:
-                if line.strip() == f"{filename}/{hash}":
-                    new_version = False
+        for line in cached:
+            if line.strip() == f"{filename}/{hash}":
+                new_version = False
+                break
 
     if new_version:
         with open(head_path, 'r') as head:
@@ -67,14 +70,42 @@ def add_to_HEAD(filename, hash):
 
     return new_version
     
+def clean_head():
+    indices = glob(".mygit/index/*/*")
+    index_file = set()
+
+    for i in indices:
+        filename = i.strip().split("/")[-2]
+        index_file.add(filename)
+
+    new_version = False
+    with open(".mygit/HEAD", 'r') as head:
+        lines = head.readlines()
+        cleaned_line = []
+
+    for line in lines:
+        line = line.strip()
+        filename = line.split("/")[0]
+
+        if filename in index_file:
+            cleaned_line.append(line+"\n")
+        else:
+            new_version = True
+    with open(".mygit/HEAD","w") as cleaned_head:
+        cleaned_head.writelines(cleaned_line)
+        
+    return new_version
 
 
 def commit():
     instage = glob(".mygit/index/*")
     if not instage:
-        print("nothing to commit")
-        return
-    change = False
+        with open(".mygit/HEAD", 'r') as head:
+            if not head.read().strip():
+                print("nothing to commit")
+                return
+            
+    change = clean_head()
 
     for file in instage:
         index = glob(file+"/*")[0]
@@ -85,6 +116,8 @@ def commit():
         if new_version:
             change = True
             shutil.copy(index,f".mygit/objects/{hash_val}")
+
+    #change = clean_head()
     if change:
         commit_log()
     else:
