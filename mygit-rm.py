6d8c5c9857435904ebf3fad7f5adfa7ef44ea77a
Ravicha <palm.ravicha@outlook.com>
2025-07-30 17:51:17 +1000

    error handling done

--- mygit-rm.py
+++ mygit-rm.py
@@ -20,13 +20,13 @@ def parse_args():
     parser.add_argument('filenames', nargs='+', help='Files to remove')
 
     args = parser.parse_args()
-
+    option = []
     if args.cached:
-        option = 'cached'
-    elif args.force:
-        option = 'force'
-    else:
-        option = 'default'
+        option.append('cached')
+    if args.force:
+        option.append('force')
+    if not args.cached and not args.force:
+        option = ['default']
 
     return option, args.filenames
 
@@ -34,27 +34,21 @@ def error_check(filename,option):
     state = mygit_util.DiffCheck.state_check(filename)
     dir_hash = mygit_util.DiffCheck.get_dir_hash(filename)
     index_hash = mygit_util.DiffCheck.get_index_hash(filename)
-    head_hash = mygit_util.DiffCheck.get_HEAD_hash(filename)
-
-    """recheck logic, still print wrong output"""
-    if not state["i=h"] and index_hash:
-        if option != "cached" and state["d=i"]:
-            print(f"mygit-rm: error: '{filename}' has staged changes in the index",file=sys.stderr)
-            exit(1)
+    #head_hash = mygit_util.DiffCheck.get_HEAD_hash(filename)
     if not index_hash:
         print(f"mygit-rm: error: '{filename}' is not in the mygit repository",file=sys.stderr)
         exit(1)
-    if not state["d=i"] and state["i=h"] and dir_hash and index_hash and option != "cached":
-        print(f"mygit-rm: error: '{filename}' in the repository is different to the working file",file=sys.stderr)
-        exit(1)
-    if not state["d=i"] and not state["i=h"] and dir_hash and index_hash:
-        if option != "cached":
+    if "force" not in option:
+        if not state["i=h"] and index_hash:
+            if "cached" not in option and state["d=i"]:
+                print(f"mygit-rm: error: '{filename}' has staged changes in the index",file=sys.stderr)
+                exit(1)
+        if not state["d=i"] and state["i=h"] and dir_hash and index_hash and "cached" not in option:
+            print(f"mygit-rm: error: '{filename}' in the repository is different to the working file",file=sys.stderr)
+            exit(1)
+        if not state["d=i"] and not state["i=h"] and dir_hash and index_hash:
             print(f"mygit-rm: error: '{filename}' in index is different to both the working file and the repository")
             exit(1)
-        else:
-            if not head_hash:
-                print(f"mygit-rm: error: '{filename}' in index is different to both the working file and the repository")
-                exit(1)
 
 def remove_from_index(filename):
     try:
@@ -77,8 +71,7 @@ if __name__ == "__main__":
     for file in files:
         check.rm_check(file)
     for file in files:
-        if option != "force":
-            error_check(file,option)
+        error_check(file,option)
         if option != "cached":
             remove_from_dir(file)
             remove_from_index(file)
