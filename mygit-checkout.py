2b88c3e5d44e4653abfb358c6281704c2c5b041f
Ravicha <palm.ravicha@outlook.com>
2025-08-01 18:29:48 +1000

    subset2: checkout 2nd draft

--- mygit-checkout.py
+++ mygit-checkout.py
@@ -4,6 +4,7 @@ from glob import glob
 import mygit_util
 import os
 from pathlib import Path
+import shutil
 
 
 args = sys.argv[1:]
@@ -53,12 +54,36 @@ def detect_conflict(target_branch): # only check tracked file
         print("mygit-checkout: error: Your changes to the following files would be overwritten by checkout:")
         print(("\n").join(conflicted_file))
         exit(1)
-        
-def switch_to(target_branch):
-    print(f"Switched to branch '{target_branch}'")
+
+def update_ref(target_branch):
     current_head = glob(".mygit/refs/branch/*")[0]
     os.remove(current_head)
     Path(f".mygit/refs/branch/{target_branch}").touch()
+
+def update_index(target_branch):
+    path = f".mygit/refs/heads/{target_branch}/HEAD"
+    new_index = dict()
+    with open(path,"r") as target:
+        for target_file in target.readlines():
+            file, hash_val = target_file.split("/")
+            new_index[file] = hash_val.strip()
+    shutil.rmtree(".mygit/index")
+    os.mkdir(".mygit/index")
+    for file,hash_val in new_index.items():
+        os.mkdir(f".mygit/index/{file}")
+        with open(f".mygit/objects/{hash_val}","r") as objects:
+            with open(f".mygit/index/{file}/{hash_val}","w") as index_file:
+                index_file.write(objects.read())
+
+
+
+
+def switch_to(target_branch):
+    print(f"Switched to branch '{target_branch}'")
+
+    update_ref(target_branch)
+    update_index(target_branch)
+
     target_head_path = glob(f".mygit/refs/heads/{target_branch}/HEAD")[0]
     with open(target_head_path, "r") as target_head_file:
         target_files = {}
